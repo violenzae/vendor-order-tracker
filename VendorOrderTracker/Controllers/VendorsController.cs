13d64e410637b9b1c12a185557de73bbc35475a8
Rachel Schieferstein <violenza@gmail.com>
2020-03-13 11:24:21 -0700

    adding vendor add and view pages

--- VendorOrderTracker/Controllers/VendorsController.cs
+++ VendorOrderTracker/Controllers/VendorsController.cs
@@ -12,5 +12,16 @@ namespace VendorOrderTracker.Controllers
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+    return View();
+    }
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+        Vendor newVendor = new Vendor(vendorName, vendorDescription);
+        return RedirectToAction("Index");
+    }
   }
 }
