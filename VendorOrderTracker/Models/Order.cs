21ef68b6f7fd4575373cc7a80a6d075773506850
Rachel Schieferstein <violenza@gmail.com>
2020-03-13 11:09:47 -0700

    adding models

---
+++ VendorOrderTracker/Models/Order.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+
+namespace VendorOrderTracker.Models
+{
+  public class Order
+  {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Price { get; set; }
+    public string Date { get; set; }
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> {};
+
+    public Order (string title, string description, string price, string date)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = date;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+  }
+}
